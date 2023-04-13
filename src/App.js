f3407d4ed1f9c8e5b4dfe07ed0125b9654154444
CauveryRaja <cauveryraja25@gmail.com>
2023-04-13 08:28:08 +0530

    Add Header and Footer with UI constants

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Greeting } from './App.style';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      <Greeting>Hello, React</Greeting>
+      <Header />
+      <Greeting>Hello, Welcome to React</Greeting>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import { Container, Title } from "./Header.styles";
+
+const Header = () => {
+    return <Container>
+        <Title>React app</Title>
+    </Container>
+};
+
+export default Header;
